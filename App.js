776ac3b32a0f827933d8d0925ae52a695e285fbe
arya.shrivastava <arya.shrivastava@kollie.ai>
2021-01-04 20:07:44 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,81 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {React, useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import Agenda from 'react-native-calendars';
+import {Card, Avatar} from 'react-native-paper';
+
+const App = () => {
+  const [items, setItems] = useState({});
+
+  const timeToString = (time) => {
+    const date = new Date(time);
+    //console.log(date);
+    return date.toISOString().split('T')[0];
+  };
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        //console.log(time);
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text>{item.name}</Text>
+              <Avatar.Text label="J" />
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={'2017-05-16'}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+};
+
+export default App;
